5524076285f9649a4c9927eca83adc6c3969225b
germtb <germtf@gmail.com>
2016-02-25 00:22:20 +0000

    Fix linter errors

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,4 @@
 import jsdom from 'jsdom';
-import chai from 'chai';
-import chaiImmutable from 'chai-immutable';
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
